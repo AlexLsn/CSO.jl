12067562dac063478d8ba4e89fe3e89f9ad8b26f
AlexLsn <lansonalexandrine@gmail.com>
2021-06-15 17:04:52 +0200

    test passed

---
+++ test/dataProcessing/RainYday_test.jl
@@ -0,0 +1,30 @@
+#test passed
+
+@testset "RainYday.jl" begin
+    data_surv = DataFrame(CSV.File("C:\\Users\\Alexandrine\\cso_raw.csv"))
+    data_trudeau = DataFrame(CSV.File("C:\\Users\\Alexandrine\\Montreal-Trudeau_15min_precipitations.csv"))
+    data = innerjoin(data_surv, data_trudeau, on=:Date)
+
+    notInc = 0
+    if data[1, :d24h] == 1
+        notInc = 1
+    end
+    for i in 2:nrow(data) 
+        if data[i-1, :d24h] .> 0 && year(data[i,:].Date) .!= year(data[i-1,:].Date)
+            notInc = notInc + 1
+        end
+    end
+    
+    transformedDF = addColRainYday!(data, "d24h", "RainYday", 5)
+    nb1 = sum(transformedDF.RainYday)
+    
+
+    #New column contains only 0's and 1'
+    @test unique(transformedDF.RainYday) in [0, 1, [0,1]]
+
+    #The number of 1 equals the number of positive precipitations but doesn't include the first day of the year
+    @test count(transformedDF[:, :d24h] .> 0.0) == (nb1 + notInc)
+
+    #Ancient DF has been changed
+    @test nrow(data) == nrow(transformedDF)   
+end
